cb90f7060c079b17fd7375b31e6b96d06a35f767
Marko <marko.krtolica@conradconnect.de>
2020-02-24 11:22:49 +0100

    Removes Email config and place it to the local_settings on the server

--- contacts/views.py
+++ contacts/views.py
@@ -28,13 +28,13 @@ def contact(request):
     contact.save()
 
     # Send email
-    # send_mail(
-    #   'Property Listing Inquiry',
-    #   'There has been an inquiry for ' + listing_title + '. Sign into the amdin panel for more info.',
-    #   'your_email',
-    #   [realtor_email],
-    #   fail_silently=False
-    # )
+    send_mail(
+      'Property Listing Inquiry',
+      'There has been an inquiry for ' + listing_title + '. Sign into the admin panel for more info.',
+      'markokrtolica1989@gmail.com',
+      [realtor_email],
+      fail_silently=False
+    )
 
     messages.success(request, 'Your request has been submitted, a realtor will get back to you soon')
     return redirect('/listings/' + listing_id)
